a8e3e3f781fdff6bfe60f87bf9120b5515f31b64
aniketkumargiri <aniket.kumar.giri2707@gmail.com>
2024-04-10 09:25:32 +0530

    Deployed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chatcraze-production.onrender.com/", {
+      const socket = io("https://chatcraze-ani.onrender.com/", {
         query: {
           userId: authUser._id,
         },
